adfbb088873a7546f71ea7f732bc6e8a80418a00
Jordan Nguyen <j.nguyen.app@gmail.com>
2026-07-21 12:51:57 -0700

    Added the rest of the planned pipeline methods and documentation

--- src/pipeline.js
+++ src/pipeline.js
@@ -2,7 +2,6 @@ import fs from "fs";
 import path from "path";
 import ffmpeg from "fluent-ffmpeg";
 import pc from "picocolors";
-import { fileTypeFromFile } from "file-type";
 
 export default class VideoPipeline {
     /**
@@ -17,6 +16,10 @@ export default class VideoPipeline {
 		this.tempDirectory = "./normalized";
     }
 
+    /**
+     * Sets the output target resolution that files will all have to be normalized to match
+     * @param {string} resolution Resolution formatted as #x#, e.g. "1920x1080", "3840x2160"
+     */
     setResolution(resolution) {
         const [ w, h ] = resolution.split("x");
 		this.targetWidth = parseInt(w, 10);
@@ -24,19 +27,70 @@ export default class VideoPipeline {
         this.resolution = resolution;
     }
 
+    /**
+     * Sets the output target frame rate that files will all have to be normalized to match
+     * @param {number} frameRate Positive integer representing frames per second
+     */
     setFrameRate(frameRate) {
         this.targetFPS = frameRate;
     }
 
-    scanFiles() {
-
+    /**
+     * Sets the length in seconds that image inputs will be looped to match in the output file
+     * @param {number} seconds Seconds each image file will take in the final output video file
+     */
+    setImageLength(seconds) {
+        this.imageLength = seconds;
     }
 
+    /**
+     * Returns a dynamic resolution of the most common input resolution to limit normalization needed
+     * @returns {string} Resolution string formatted as #x#, e.g. "1920x1080", "3840x2160"
+     */
     getSmartResolution() {
 
     }
 
+    /**
+     * Returns a dynamic frame rate of the most common input frame rate to limit normalization needed
+     * @returns {number} Positive integer representing most frequent frames per second of video
+     */
     getSmartFrameRate() {
+
+    }
+
+    /**
+     * Recursively appends file paths to this.assets array in the initialized input directory
+     * @param {import("@clack/prompts").spinner} spinner Clack spinner to show progress over time
+     * @returns {void} Appends objects representing input file meta data into this.assets
+     */
+    async scanFiles(spinner) {
+
+    }
+
+    /**
+     * Sorts the assets array given the provided preset string, changing the order files are stitched
+     * @param {string} method Preset sort method described by the hard coded function methods below
+     * @returns {Array<Object>} Returns the sorted this.assets array of objects
+     */
+    sortFiles(method) {
+
+    }
+    
+    /**
+     * Normalize all assets with the given output parameters so the final stitching can use -c copy
+     * @param {import("@clack/prompts").spinner} spinner Clack spinner to show progress over time
+     */
+    async normalizeFiles(spinner) {
+
+    }
+
+    /**
+     * Stitches the normalized video files together into a final output video using -c copy flag
+     * ffmpeg -c copy copies the bits directly and does not need to re-encode the video if used
+     * @param {import("@clack/prompts").spinner} spinner Clack spinner to show progress over time
+     */
+    async stitchFiles(spinner) {
 
     }
 }
